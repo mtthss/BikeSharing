bd01b5e0407c585b5a6ea8101ed6652202285609
mtthss <matteo.hessel@mail.polimi.it>
2015-03-17 14:17:22 +0000

    rf changed

--- Preprocessing/FeatureEngineering.py
+++ Preprocessing/FeatureEngineering.py
@@ -5,6 +5,7 @@
 import pandas as pd
 from datetime import datetime
 import numpy as np
+import pickle
 np.set_printoptions(threshold=np.nan)
 
 
@@ -12,16 +13,16 @@ np.set_printoptions(threshold=np.nan)
 # Reading Data #
 ################
 print "---------------"
-print "Reading...!"
-df_train = pd.read_csv('train.csv')
-df_test = pd.read_csv('test.csv')
+print "Reading..."
+df_train = pd.read_csv('../Data/train.csv')
+df_test = pd.read_csv('../Data/test.csv')
 
 
 #######################
 # Engineer Timestamps #
 #######################
-print "---------------"
-print "Engineering timestamps!"
+print "\n---------------"
+print "Engineering timestamps..."
 def transform_timestamps(df):
 
     i = 0
@@ -39,8 +40,55 @@ train = transform_timestamps(df_train)
 test = transform_timestamps(df_test)
 
 
+##############################
+# Combining weather features #
+##############################
+print "\n---------------"
+print "Combining weather features..."
+def my_poly(df):
+
+    i = 0
+    for temp in df['atemp']:
+
+        i += 1
+        hu = df.loc[i-1, 'humidity']
+        ws = df.loc[i-1, 'windspeed']
+        tempTimesHumidity = temp*hu
+        tempTimesWindspeed = temp*ws
+        humidityTimesWindSpeed = hu*ws
+        tempDivWindspeed = float(temp)/ws
+        df.loc[i-1, 'tempTimesHumidity'] = tempTimesHumidity
+        df.loc[i-1, 'tempTimesWindspeed'] = tempTimesWindspeed
+        df.loc[i-1, 'humidityTimesWindspeed'] = humidityTimesWindSpeed
+        df.loc[i-1, 'tempDivWindspeed'] = tempDivWindspeed
+
+    return df
+
+train = my_poly(df_train)
+test = my_poly(df_test)
+
+
+#################
+# Insert memory #
+#################
+
+# previous day temp and humidity
+
+
+##########
+# Pickle #
+##########
+print '\n------------------'
+print 'Pickling...'
+all_cols = ['date','time', 'season', 'holiday', 'workingday', 'weather', 'temp', 'atemp', 'humidity', 'windspeed',
+            'tempTimesHumidity', 'tempTimesWindspeed', 'humidityTimesWindspeed', 'tempDivWindspeed']
+pickle.dump(all_cols, open("../Data/all_cols.pkl", "wb"))
+pickle.dump(train, open("../Data/train.pkl", "wb"))
+pickle.dump(test, open("../Data/test.pkl", "wb"))
+
+
 ##########
 # Ending #
 ##########
-print "---------------"
+print "\n---------------"
 print "Done!"
