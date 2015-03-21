05d5b5a24bf3231d84265c32f457c622ebb926a5
hmourit <hmourit@gmail.com>
2015-03-21 17:31:40 +0000

    Expand feature engineering.

--- preprocessing/feature_engineering.py
+++ preprocessing/feature_engineering.py
@@ -2,6 +2,10 @@ from __future__ import division, print_function
 
 import pandas as pd
 
+ENGINEERED_FEATS = ['year', 'year-2011', 'month', 'day', 'weekday', 'hour', 'dummy_season', 'temp * humidity',
+                    'temp*windspeed', 'humidity*windspeed', 'temp/windspeed', 'dummy_weather', 'dummy_year',
+                    'dummy_month', 'dummy_day', 'dummy_weekday', 'dummy_hour']
+
 
 def _compute_features(data, features):
     df = pd.DataFrame()
@@ -10,16 +14,31 @@ def _compute_features(data, features):
             df[feat] = data[feat]
         elif feat == 'year':
             df[feat] = pd.to_datetime(data['datetime']).apply(lambda x: x.year)
+        elif feat == 'dummy_year':
+            df = pd.concat(
+                [df, pd.get_dummies(pd.to_datetime(data['datetime']).apply(lambda x: x.year), prefix='year')], axis=1)
         elif feat == 'year-2011':
             df[feat] = pd.to_datetime(data['datetime']).apply(lambda x: x.year - 2011)
         elif feat == 'month':
             df[feat] = pd.to_datetime(data['datetime']).apply(lambda x: x.month)
+        elif feat == 'dummy_month':
+            dummies = pd.get_dummies(pd.to_datetime(data['datetime']).apply(lambda x: x.month), prefix='month')
+            df = pd.concat([df, dummies], axis=1)
         elif feat == 'day':
             df[feat] = pd.to_datetime(data['datetime']).apply(lambda x: x.day)
+        elif feat == 'dummy_day':
+            dummies = pd.get_dummies(pd.to_datetime(data['datetime']).apply(lambda x: x.day), prefix='day')
+            df = pd.concat([df, dummies], axis=1)
         elif feat == 'weekday':
             df[feat] = pd.to_datetime(data['datetime']).apply(lambda x: x.weekday())
+        elif feat == 'dummy_weekday':
+            dummies = pd.get_dummies(pd.to_datetime(data['datetime']).apply(lambda x: x.weekday()), prefix='weekday')
+            df = pd.concat([df, dummies], axis=1)
         elif feat == 'hour':
             df[feat] = pd.to_datetime(data['datetime']).apply(lambda x: x.hour)
+        elif feat == 'dummy_hour':
+            dummies = pd.get_dummies(pd.to_datetime(data['datetime']).apply(lambda x: x.hour), prefix='hour')
+            df = pd.concat([df, dummies], axis=1)
         elif feat == 'dummy_season':
             df['spring'] = (data['season'] == 1).astype(int)
             df['summer'] = (data['season'] == 2).astype(int)
